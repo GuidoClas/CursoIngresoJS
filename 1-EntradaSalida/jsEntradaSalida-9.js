8e1e15805e94a894ef48a140928c152fff6a8e7b
alumno <alumno>
2020-02-05 12:10:08 -0300

    Clase 2

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,14 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+var importe;
+var resultado;
+var aumento;
 
-	
+importe = document.getElementById("sueldo").value;
+
+
+resultado = parseInt(importe) * 1.1
+
+document.getElementById("resultado").value = resultado;
 }
