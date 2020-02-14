83b18c5c95479a31f723e3119631d8bee4445c5e
unknown <guido-clas@hotmail.com>
2020-02-13 22:28:03 -0300

    Terminado Switch

--- 3-InstruccionSwitch/jsInstruccionSwitch(5).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(5).js
@@ -1,11 +1,22 @@
 function mostrar()
 {
-//tomo la edad  
+//tomo la hora  
 var laHora = document.getElementById('hora').value;
 
 //alert (laHora);
 	
-	
+switch (parseInt(laHora)) {
+    case 7:
+    case 8:
+    case 9:
+    case 10:
+    case 11:
+        alert("Es la mañana!")                
+        break;
+
+    default:
+        break;
+}	
 
 
 
