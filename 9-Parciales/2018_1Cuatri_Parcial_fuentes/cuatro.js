ed92cc4305cb24ed2b17311d656da77837327871
unknown <guido-clas@hotmail.com>
2020-03-01 22:29:03 -0300

    terminado For

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -2,22 +2,23 @@ function mostrar()
 {
     var numero1 = prompt("Ingrese el primer numero");
     var numero2 = prompt("Ingrese el segundo numero");
-    var suma = parseInt(numero1) + parseInt(numero2);
+    
 
     if (numero1 === numero2) {
         alert(numero1 + numero2);
-    } else if (numero1 > numero2) {
-        numero1 = parseInt(numero1);
-        numero2 = parseInt(numero2);
-        alert(numero1-numero2);
-    } else if (suma > 10) {
-        numero1 = parseInt(numero1);
-        numero2 = parseInt(numero2);
-        alert("La suma es " + suma + " y supero al 10");
     } else if (numero1 < numero2) {
         numero1 = parseInt(numero1);
         numero2 = parseInt(numero2);
-        alert(numero1+numero2);
-    }
+        var suma = numero1 + numero2;
+        alert(suma);
+            if (suma > 10) {
+                alert("La suma es " + suma + " y supera al 10");
+            }
+    } else if (numero1 > numero2) {
+        numero1 = parseInt(numero1);
+        numero2 = parseInt(numero2);
+        var resta = numero1-numero2;
+        alert(resta);
+    } 
     
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,17 +1,27 @@
 function mostrar()
 {
-     var nota = prompt("Ingresa la nota");
-     var sexo = prompt("Ingresa el sexo (f o m)");
-     var contadorAlumnos = 0;
-     
-     
-     while (contadorAlumnos < 5){
-         while (isNaN(nota) || nota > 10 || nota < 0) {
+    var nota = prompt("Ingresa la nota");
+    var sexo = prompt("Ingresa el sexo (f o m)");
+    var contadorAlumnos = 0;
+    var acumuladorNota = 0;
+    var promedio;
+
+    while (contadorAlumnos < 5){
+        while (isNaN(nota) || nota > 10 || nota < 0) {
              nota = prompt("Error, ingresa una nota valida");
-         }
-         while (sexo != "f" && sexo != "m") {
+        }
+        while (sexo != "f" && sexo != "m") {
              sexo = prompt("Error, ingresa un sexo valido")
-         }
-         contadorAlumnos++;
-     }
+        }
+        nota = prompt("ingresa la siguiente nota");
+        sexo = prompt("ingresa el siguiente sexo");
+        nota = parseInt(nota);
+        contadorAlumnos++;
+        acumuladorNota+=nota
+        console.log("acumulador: " + acumuladorNota);
+        console.log("Nota: " + nota);
+    }
+    promedio = acumuladorNota/5;
+    alert(promedio);
+    
 }
